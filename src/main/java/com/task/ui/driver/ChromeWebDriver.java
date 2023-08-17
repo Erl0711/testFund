2bb9e8827ce9e3cc9fd2732e52e1e2af1835b124
Chadaev Erlan <bekowa123@gmail.com>
2023-08-18 00:21:36 +0600

    FirstCommit

---
+++ src/main/java/com/task/ui/driver/ChromeWebDriver.java
@@ -0,0 +1,43 @@
+package com.task.ui.driver;
+
+import com.digital.config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class ChromeWebDriver implements  Waits{
+
+   private static WebDriver driver;
+    public static WebDriver loadChromeDriver(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
+
+
+        if(Boolean.getBoolean(ConfigReader.getProperty("headless"))){
+
+
+
+        }
+
+        driver = new ChromeDriver(options);
+        ChromeWebDriver chromeWebDriver = new ChromeWebDriver();
+        chromeWebDriver.setUpImplicitWait(driver);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    @Override
+    public WebDriver.Timeouts setUpImplicitWait() {
+        return null;
+    }
+
+    @Override
+    public WebDriver.Timeouts setUpImplicitWait(WebDriver driver) {
+        return driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Waits.implicitWaitTime));
+    }
+}
---
+++ src/main/java/com/task/ui/driver/Driver.java
@@ -0,0 +1,49 @@
+package com.task.ui.driver;
+
+import com.digital.config.ConfigReader;
+import org.openqa.selenium.WebDriver;
+
+
+public class Driver {
+
+    private Driver() {
+        // Singleton pattern
+    }
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        if (driver == null) {
+            switch (ConfigReader.getProperty("browser").toLowerCase()) {
+                case "chrome":
+                    driver = ChromeWebDriver.loadChromeDriver();
+                    break;
+                case "edge":
+                    driver = EdgeWebDriver.loadEdgeDriver();
+                    break;
+                case "firefox":
+                    driver = FirefoxWebDriver.loadFirefoxDriver();
+                    break;
+                case  "sauce_mac_safari":
+                    driver = SauceLabsMacFireFox.loadSauceMacFireFoxDriver();
+                    break;
+                default:
+                    throw new RuntimeException("You provided wrong browser name");
+            }
+        }
+        return driver;
+    }
+
+
+    public static void closeDriver(){
+        try {
+            if (driver != null){
+                driver.close();
+                driver.quit();
+                driver = null;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
